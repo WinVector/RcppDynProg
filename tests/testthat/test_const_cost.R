a775c3361015418dcdace78ba35fd30cb36cb81b
John Mount <jmount@win-vector.com>
2018-12-29 13:47:32 -0800

    fix const-weighting more tests

---
+++ tests/testthat/test_const_cost.R
@@ -0,0 +1,16 @@
+library('RcppDynProg')
+
+context("test_const_cost")
+
+
+test_that("test_const_cost: test scoring", {
+  y <- c(1, 2, 1, -1, 4, 5, 10)
+  w <- 1 + numeric(length(y))
+  c1 <- const_cost(y, w, 0, length(y)-1)
+  
+  out_est <- (sum(y*w) - y*w)/(sum(w) - w)
+  c2 <- sum(w*(y-out_est)^2)
+  
+  testthat::expect_true(abs(c1-c2)<=1e-5)
+})
+  
---
+++ tests/testthat/test_lin_cost.R
@@ -0,0 +1,24 @@
+library('RcppDynProg')
+
+context("test_lin_cost")
+
+
+test_that("test_lin_cost: test scoring", {
+  y <- c(1, 2, 1, -1, 4, 5, 10)
+  x <- seq_len(length(y))
+  w <- 1 + numeric(length(y))
+  c1 <- lin_cost(x, y, w, 0, length(y)-1)
+  
+  d <- data.frame(x = x, y = y)
+  out_est <- vapply(
+    seq_len(length(y)),
+    function(i) {
+      m <- lm(y~x, data = d[-i, ], weights = w[-i])
+      predict(m, newdata = d[i, ])
+    }, numeric(1))
+                    
+  c2 <- sum(w*(y-out_est)^2)
+  
+  testthat::expect_true(abs(c1-c2)<=1e-3)
+})
+  
