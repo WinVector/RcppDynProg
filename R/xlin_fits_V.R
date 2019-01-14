0ed74c476ddce27b059578e451aedf5cb5486e99
John Mount <jmount@win-vector.com>
2019-01-14 08:42:47 -0800

    remove semi-colons from vectorized example

--- R/xlin_fits_V.R
+++ R/xlin_fits_V.R
@@ -17,30 +17,30 @@
 #' @export
 #' 
 xlin_fits_V <- function(x, y, w) {
-  n = length(y);
+  n = length(y)
   # build fitting data
-  regularization = 1.0e-5;
-  xx_0_0 = numeric(n) + sum(w*1);
-  xx_1_0 = numeric(n) + sum(w*x);
-  xx_0_1 = numeric(n) + sum(w*x);
-  xx_1_1 = numeric(n) + sum(w*x*x);
-  xy_0 = numeric(n) + sum(w*y);
-  xy_1 = numeric(n) + sum(w*x*y);
-  xx_1_0 = xx_1_0 + regularization;
-  xx_0_1  = xx_0_1  + regularization;
+  regularization = 1.0e-5
+  xx_0_0 = numeric(n) + sum(w*1)
+  xx_1_0 = numeric(n) + sum(w*x)
+  xx_0_1 = numeric(n) + sum(w*x)
+  xx_1_1 = numeric(n) + sum(w*x*x)
+  xy_0 = numeric(n) + sum(w*y)
+  xy_1 = numeric(n) + sum(w*x*y)
+  xx_1_0 = xx_1_0 + regularization
+  xx_0_1  = xx_0_1  + regularization
   # pull out k'th observation
-  xxk_0_0 = xx_0_0 - w*1;
-  xxk_1_0 = xx_1_0 - w*x;
-  xxk_0_1 = xx_0_1 - w*x;
-  xxk_1_1 = xx_1_1 - w*x*x;
-  xyk_0 = xy_0 - w*y;
-  xyk_1 = xy_1 - w*x*y;
+  xxk_0_0 = xx_0_0 - w*1
+  xxk_1_0 = xx_1_0 - w*x
+  xxk_0_1 = xx_0_1 - w*x
+  xxk_1_1 = xx_1_1 - w*x*x
+  xyk_0 = xy_0 - w*y
+  xyk_1 = xy_1 - w*x*y
   # solve linear system
-  det = xxk_0_0*xxk_1_1 - xxk_0_1*xxk_1_0;
-  c0 = (xxk_1_1*xyk_0 - xxk_0_1*xyk_1)/det;
-  c1 = (-xxk_1_0*xyk_0 + xxk_0_0*xyk_1)/det;
+  det = xxk_0_0*xxk_1_1 - xxk_0_1*xxk_1_0
+  c0 = (xxk_1_1*xyk_0 - xxk_0_1*xyk_1)/det
+  c1 = (-xxk_1_0*xyk_0 + xxk_0_0*xyk_1)/det
   # form estimate
-  y_est = c0 + c1*x;
-  return(y_est);
+  y_est = c0 + c1*x
+  return(y_est)
 }
 
