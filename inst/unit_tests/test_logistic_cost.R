39a75a9b10b6b261495b074eb1d7221b945288d2
John Mount <jmount@win-vector.com>
2019-02-02 11:22:03 -0800

    stengthen test

--- inst/unit_tests/test_logistic_cost.R
+++ inst/unit_tests/test_logistic_cost.R
@@ -3,8 +3,10 @@ test_logistic_cost <- function() {
   x <- c(1, 2, 3, 4, 5, 6, 7)
   y <- c(0, 0, 1, 0, 1, 1, 0)
   w <- c(1, 1, 1, 1, 1, 1, 1)
+  il <- numeric(length(x))
   
   for(k in wrapr::seqi(0, 4)) {
+    m1 <- logistic_solve1(x, y, w, il, 0, k, -1)
     lf <- logistic_fits(x, y, w, 0, k)
     RUnit::checkTrue(is.numeric(lf))
     RUnit::checkEquals(k+1, length(lf))
@@ -14,10 +16,14 @@ test_logistic_cost <- function() {
     if(k>=3) {
       d <- data.frame(x = x[1:(k+1)], y  = y[1:(k+1)])
       m <- glm(y~x, data=d, family = binomial)
+      cm <- as.numeric(coef(m))
+      diff1 <- max(abs(m1-cm))
+      msg1 <- print(paste("coef problem", k, diff, wrapr::map_to_char(m1), wrapr::map_to_char(cm)))
+      RUnit::checkTrue(diff<=1e-3, msg = msg1)
       p <- as.numeric(predict(m, newdata = d, type = "link"))
       diff <- max(abs(lf-p))
       if(diff>1e-3) {
-        print(paste(k, diff))
+        print(paste("link problem", k, diff))
         print(lf)
         print(p)
         RUnit::checkTrue(FALSE)
